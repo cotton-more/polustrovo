07a2088f69c16f69ae17298aa328263cf067c4ad
Ivan <VanSanblch@gmail.com>
2017-04-27 22:20:32 +0300

    Use locker for broadcasting command

--- src/Cilex/Command/BroadcastScreenshotsCommand.php
+++ src/Cilex/Command/BroadcastScreenshotsCommand.php
@@ -3,13 +3,15 @@
 namespace Cilex\Command;
 
 use App\Service\Broadcast\BroadcastService;
-use App\Service\Notifier\PushbulletNotifier;
 use Cilex\Provider\Console\Command;
+use Symfony\Component\Console\Command\LockableTrait;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class BroadcastScreenshotsCommand extends Command
 {
+    use LockableTrait;
+
     public function configure()
     {
         $this->setName('broadcast:screenshots');
@@ -17,6 +19,11 @@ class BroadcastScreenshotsCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
+        if (!$this->lock()) {
+            $output->writeln($this->getName(). ' is already running');
+            return;
+        }
+
         /** @var BroadcastService $service */
         $service = $this->getContainer()->get('broadcast');
 
